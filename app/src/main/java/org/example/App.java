70b3d4f13b1b98b7641e0d712fdd7a7d237c1ceb
Elimka777 <elimazholdubaeva@gmail.com>
2025-08-24 21:31:19 -0500

    Done

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,20 +1,28 @@
 package org.example;
 
 public class App {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Converter converter = new Converter();
 
-        int[] testOunces = {0, 1, 2, 16, 24, 32};
+        // Simulated user input
+        int ounces = 62; //change this value to test different inputs
 
-        System.out.println("Ounces to Pounds:");
-        for (int oz : testOunces) {
-            System.out.println(oz + " oz = " + converter.toPounds(oz));
-        }
+        // welcome prompt
+        System.out.println("Welcome to the Ounce Conversion Program!\n");
 
-        System.out.println("\nOunces to Pounds and Ounces:");
-        for (int oz : testOunces) {
-            System.out.println(oz + " oz = " + converter.toPoundsAndOunces(oz));
-        }
+        // confirming input
+        System.out.println("How many ounces do you have? " + ounces);
+        System.out.println("Thank you! Converting ounces to pounds.\n");
+
+        // printing converted values
+        System.out.println(ounces + " oz is equivalent to " 
+                           + converter.toPoundsAndOunces(ounces) + ".");
+        System.out.println(ounces + " oz is equivalent to " 
+                           + converter.toPounds(ounces) + ".\n");
+
+        // exiting message
+        System.out.println("Thank you for using the OCP!");
     }
 }
+
 
